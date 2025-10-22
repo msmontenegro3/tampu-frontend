e501ba219aff4cb22372b349c19e44346214187c
msmontenegro3 <mirta.s.montenegro.l@gmail.com>
2025-10-21 23:21:39 -0500

    Login with validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../core/auth.service';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,35 +17,60 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-
   errorMessage = '';
   loading = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  //Formulario reactivo
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.pattern(/^(?=.*[!@#$%^&*(),.?":{}|<>]).{4,}$/),
+      ],
+    ],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   onSubmit() {
     this.loading = true;
     this.errorMessage = '';
 
-    this.authService
-      .login({ email: this.email, password: this.password })
-      .subscribe({
-        next: (res) => {
-          this.loading = false;
-          this.authService.saveToken(res.access_token);
+    const { email, password } = this.loginForm.value;
 
-          // Identifica el rol
-          const payload = JSON.parse(atob(res.access_token.split('.')[1]));
-          if (payload.rol === 'docente') this.router.navigate(['/teacher']);
-          else this.router.navigate(['/student']);
-        },
+    this.authService.login({ email, password }).subscribe({
+      next: (res) => {
+        this.loading = false;
+        this.authService.saveToken(res.access_token);
 
-        error: (err) => {
-          this.loading = false;
-          this.errorMessage = `Error: ${err}`;
-        },
-      });
+        // Identifica el rol
+        const payload = JSON.parse(atob(res.access_token.split('.')[1]));
+        if (payload.rol === 'docente') this.router.navigate(['/teacher']);
+        else this.router.navigate(['/student']);
+      },
+
+      error: (err) => {
+        this.loading = false;
+        this.errorMessage = `Error: ${err}`;
+      },
+    });
+  }
+
+  // Getters
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 }
